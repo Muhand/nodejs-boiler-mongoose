a2af8000c11bb535e992053c368400efbd5ab62d
Muhand Jumah <muhandjumah@gmail.com>
2021-01-16 23:39:48 -0500

    Created dependency injector loader

---
+++ src/loaders/dependencyInjector.js
@@ -0,0 +1,42 @@
+"use strict"
+
+const { createContainer, asClass, asValue, InjectionMode } = require('awilix')
+
+// Config
+const config = require('../config')
+
+// Controllers
+const mainController = require(`../interfaces/api/${config.app.apiVersion}/controllers/main`)
+
+// Middlewares
+
+const containerOptions = {
+  injectionMode: InjectionMode.PROXY
+}
+
+const loadAppContainer = async ({dbConnection, expressApp}) => {
+  const container = createContainer(containerOptions)
+
+  container.register({
+    config: asValue(config),
+    mainController: asClass(mainController)
+  })
+
+  // Inject db connection
+  if (dbConnection && dbConnection != undefined) {
+    container.register({
+      dbConnection: asValue(dbConnection),
+    })
+  }
+
+  // Inject the actual express app
+  if (expressApp && expressApp != undefined) {
+    container.register({
+      app: asValue(expressApp),
+    })
+  }
+
+  return container
+}
+
+module.exports = loadAppContainer
