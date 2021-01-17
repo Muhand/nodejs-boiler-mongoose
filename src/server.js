dbea3ae4e8ef746009fa916967dddcfd79a25292
Muhand Jumah <muhandjumah@gmail.com>
2021-01-17 02:48:34 -0500

    Initialized container and setup express

--- src/server.js
+++ src/server.js
@@ -3,18 +3,30 @@
 const express = require('express')
 const http = require('http')
 const loaders = require('./loaders')
+const container = require('./container')
 const config = require('./config')
+const expressInit = require('./express')
 
 class Server {
   constructor() {
     this.app = express()
     this.server = http.createServer(this.app)
     this.app.server = this.server
+
+    this.startServer = this.startServer.bind(this)
+    this.stop = this.stop.bind(this)
   }
 
   async startServer() {
     // Load dependencies
-    await loaders.init({expressApp: this.app, config:config})
+    let modules = await loaders.init({config:config})
+    await container.init({modules})
+
+    // Start interfaces
+    // Web API interface
+    console.log(`Initializing Express...`)
+    await expressInit({app: this.app, appConfig: config.app}, modules.expressMiddlewares)
+    console.log(`Express Initialized successfully`)
 
     // Start the server
     console.log(`\nStarting Server...`);
