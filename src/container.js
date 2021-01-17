4ce4380b144c63214fb82f9557730b5f3e09a0ed
Muhand Jumah <muhandjumah@gmail.com>
2021-01-17 02:48:49 -0500

    export container initializer and Container

--- src/container.js
+++ src/container.js
@@ -3,41 +3,49 @@
 const { createContainer, asClass, asValue, InjectionMode } = require('awilix')
 
 // Config
-const config = require('../config')
+const config = require('./config')
 
 // Controllers
-const mainController = require(`../interfaces/api/${config.app.apiVersion}/controllers/main`)
+const mainController = require(`./interfaces/api/${config.app.apiVersion}/controllers/main`)
 
 // Middlewares
 
 const containerOptions = {
   injectionMode: InjectionMode.PROXY
 }
+let container = createContainer(containerOptions)
 
-const loadAppContainer = async ({dbConnection, expressApp}) => {
-  const container = createContainer(containerOptions)
-
+const init = async ({modules}) => {
   container.register({
-    container: (c) => c,
     config: asValue(config),
     mainController: asClass(mainController),
+    container: (c) => c
   })
 
-  // Inject db connection
-  if (dbConnection && dbConnection != undefined) {
-    container.register({
-      dbConnection: asValue(dbConnection),
-    })
-  }
+  if (modules) {
+    const dbConnection = modules.dbConnection
+    const expressApp = modules.expressApp
 
-  // Inject the actual express app
-  if (expressApp && expressApp != undefined) {
-    container.register({
-      app: asValue(expressApp),
-    })
+    // Inject db connection
+    if (dbConnection && dbConnection != undefined) {
+      container.register({
+        dbConnection: asValue(dbConnection),
+      })
+    }
+
+    // Inject the actual express app
+    if (expressApp && expressApp != undefined) {
+      container.register({
+        app: asValue(expressApp),
+      })
+    }
   }
 
   return container
 }
 
-module.exports = loadAppContainer()
+
+module.exports = {
+  Container: container,
+  init: init
+}
